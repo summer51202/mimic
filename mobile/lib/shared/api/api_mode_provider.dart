b5df8c82c77ce9ddc10b267f582623fa6a50af4b
Edward1_Lee <edward1_lee@asus.com>
2026-08-28 09:48:57 +0800

    chore: retire the mobile client

--- mobile/lib/shared/api/api_mode_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../config/app_config.dart';
-
-final apiModeProvider = Provider<AppApiMode>((Ref ref) {
-  return appConfig.apiMode;
-});
